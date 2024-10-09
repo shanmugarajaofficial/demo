886f29f402fa81ded02d75551fd1b963df93a2f4
shanmugaraja-linux <shanmugarajafullstackdev@gmail.com>
2024-10-09 06:48:13 +0530

    v1

---
+++ script.js
@@ -0,0 +1,25 @@
+// Reference DataTypes
+
+
+// Array
+
+
+const officestaffnum = [9940168653,48294798324,99343344];
+
+console.log(officestaffnum);
+console.log(typeof officestaffnum);
+
+
+// Object 
+
+const indhu = {
+    name: "indhu",
+    age: 21,
+    phonenumber: 90093114324,
+    staffid: 2
+}
+
+
+console.log(indhu);
+console.log(typeof indhu);
+
